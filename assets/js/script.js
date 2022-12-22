a7351b7a30b5a07a4d6c5d3f0cec48c2300595e2
Victor Hosth <kvbn147@gmail.com>
2022-12-22 21:10:20 +0000

    Correção layout, testes responsividade

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,12 +2,9 @@
 let keys = document.querySelectorAll('.key')
 
 definirHeightKeys()
-console.log('entrou na função')
+
 function definirHeightKeys(){
     let keysWidth = keys[0].clientWidth
-    console.log(keysWidth)
-
-
     for(let i in keys){
         keys[i].style = (`height:${keysWidth}px;`)
     }
